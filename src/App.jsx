caa81516bf9c1b4daf7e69a2674ddac434bac28b
sanduni4822 <sanduniwathsala0123@gmail.com>
2025-03-13 16:06:31 +0530

    Removed GradeSelectionPage.jsx file & Updated App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import LoginPage from "./pages/LoginPage/LoginPage";
-import GradeSelectionPage from "./pages/GradeSelection/GradeSelectionPage";
 
 export default function App() {
   return (
@@ -9,7 +8,6 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<LoginPage />} /> {/* Default (Login) */}
-          <Route path="grade-selection" element={<GradeSelectionPage />} /> {/* Grade Selection */}
         </Route>
       </Routes>
     </Router>
